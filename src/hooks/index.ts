22579ab33e38033e26fc2023913243bf9e108b76
luchobonatti <luciano@bootnode.dev>
2025-05-20 17:01:51 -0300

    feat: adds exports getPool/useGetPool

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
+export { useGetPool } from "@/hooks/useGetPool";
 export { useGetQuote } from "@/hooks/useGetQuote";
---
+++ src/types/hooks/index.ts
@@ -0,0 +1,2 @@
+export * from "@/types/hooks/useGetPool";
+export * from "@/types/hooks/useGetQuote";
